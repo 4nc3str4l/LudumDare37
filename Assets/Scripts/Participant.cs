1b159094c9051cf06ab3e1df8559f8969d817a03
4nc3str4l <cmuri_10@hotmail.com>
2016-12-10 16:12:05 +0100

    Fixed AI bug

--- Assets/Scripts/Participant.cs
+++ Assets/Scripts/Participant.cs
@@ -27,6 +27,7 @@ public class Participant : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
     }
 
     public void Shoot()
